c455d8fba1b5c0884505a41d49c937df11ce2a7d
marben431 <marben431@E29613.edu.linkoping.se>
2014-08-28 14:51:09 +0200

    första

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $x = 18;
+            
+            switch($x){
+                case 1: echo "Albin är kass";
+                    break;
+                case 2: echo "Martin är bäst";
+                    break;
+                case 3: echo "Truls är bättre än kassbin";
+                    break;
+                default: echo "Martin är bäst liksom typ bara.... jag menar så";
+            }
+        ?>
+    </body>
+</html>
